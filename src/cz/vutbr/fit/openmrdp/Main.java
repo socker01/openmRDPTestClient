e22d8063adc9a1aa7e0bd1e007b174caf7e8474e
socker <jk.socker@gmail.com>
2018-05-22 23:11:38 +0200

    Changes after testing

--- src/cz/vutbr/fit/openmrdp/Main.java
+++ src/cz/vutbr/fit/openmrdp/Main.java
@@ -2,56 +2,135 @@ package cz.vutbr.fit.openmrdp;
 
 import cz.vutbr.fit.openmrdp.api.OpenmRDPClientAPI;
 import cz.vutbr.fit.openmrdp.api.OpenmRDPClientApiImpl;
-import cz.vutbr.fit.openmrdp.logger.MrdpTestLoggerImpl;
+import cz.vutbr.fit.openmrdp.exceptions.NetworkCommunicationException;
+import cz.vutbr.fit.openmrdp.exceptions.QuerySyntaxException;
+import cz.vutbr.fit.openmrdp.logger.MrdpDummyLoggerImpl;
+import cz.vutbr.fit.openmrdp.logger.MrdpLogger;
 import cz.vutbr.fit.openmrdp.messages.OperationType;
 
+import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Enumeration;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        Parameters params = parseArguments(args, args.length);
+    public static void main(String[] args) {
 
-        OpenmRDPClientAPI clientAPI = new OpenmRDPClientApiImpl(getLocalIpAddress() + ":27741/", new MrdpTestLoggerImpl());
-        if (params.getOperationType() == OperationType.LOCATE) {
-            String resourceURI = clientAPI.locateResource(params.getResource());
-            System.out.println(resourceURI);
-        } else {
-            String resourceURI = clientAPI.identifyResource(params.getResource());
-            System.out.println(resourceURI);
+        MrdpLogger logger = new MrdpDummyLoggerImpl();
+        Parameters params = parseArguments(args, args.length);
+        boolean secure = params.getUserName() != null;
+
+        if (!checkParams(params)) {
+            System.out.println("Invalid parameters combination");
+        }
+
+        String localAddress = null;
+        try {
+            localAddress = getLocalIpAddress();
+        } catch (Exception e) {
+            logger.logError(e.getMessage());
+        }
+        OpenmRDPClientAPI clientAPI = new OpenmRDPClientApiImpl(localAddress + ":27741/", new MrdpDummyLoggerImpl(), true);
+        try {
+            if (params.getOperationType() == OperationType.LOCATE) {
+                String resourceURI;
+                if (secure) {
+                    resourceURI = clientAPI.locateResource(params.getResource(), params.getUserName(), params.getPassword());
+                } else {
+                    resourceURI = clientAPI.locateResource(params.getResource());
+                }
+                printResult(resourceURI);
+            } else {
+                String query = null;
+                if (params.getFileName() != null) {
+                    try {
+                        query = loadQueryFromFile(params.getFileName());
+                    } catch (IOException e) {
+                        logger.logError(e.getMessage());
+                    }
+                } else {
+                    query = params.getResource();
+                }
+                String resourceURI;
+                if (secure) {
+                    resourceURI = clientAPI.identifyResource(query, params.getUserName(), params.getPassword());
+                } else {
+                    resourceURI = clientAPI.identifyResource(query);
+                }
+
+                printResult(resourceURI);
+            }
+        } catch (NetworkCommunicationException | QuerySyntaxException e) {
+
+            System.out.println("Servers don't have any information about this resource.");
         }
     }
 
+    private static boolean checkParams(Parameters params) {
+        if ((params.getUserName() != null && params.getPassword() == null)
+                || (params.getUserName() == null && params.getPassword() != null)) {
+            return false;
+        }
+
+        if (params.getResource() != null && params.getFileName() != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    private static void printResult(String result) {
+        if (result == null) {
+            System.out.println("Servers don't have any information about this resource.");
+        } else {
+            System.out.println(result);
+        }
+    }
+
+    private static String loadQueryFromFile(String fileName) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(fileName));
+
+        return new String(encoded, "UTF-8");
+    }
+
     private static Parameters parseArguments(String[] args, int argv) {
-        OperationType operationType = null;
-        String resource = null;
-        String endpoint = null;
+        Parameters.Builder parametersBuilder = new Parameters.Builder();
         for (int i = 0; i < argv; i = i + 2) {
             String arg = args[i];
             switch (arg) {
                 case "-i":
+                    parametersBuilder.withOperationType(OperationType.IDENTIFY);
+                    parametersBuilder.withResource(args[i + 1]);
+                    break;
                 case "-if":
-                    operationType = OperationType.IDENTIFY;
-                    resource = args[i + 1];
+                    parametersBuilder.withOperationType(OperationType.IDENTIFY);
+                    parametersBuilder.withFileName(args[i + 1]);
                     break;
                 case "-l":
-                    operationType = OperationType.LOCATE;
-                    resource = args[i + 1];
+                    parametersBuilder.withOperationType(OperationType.LOCATE);
+                    parametersBuilder.withResource(args[i + 1]);
                     break;
                 case "-e":
-                    endpoint = args[i + 1];
+                    parametersBuilder.withEndpoint(args[i + 1]);
+                    break;
+                case "-u":
+                    parametersBuilder.withUserName(args[i + 1]);
+                    break;
+                case "-p":
+                    parametersBuilder.withPassword(args[i + 1]);
                     break;
                 default:
                     throw new IllegalArgumentException("Wrong input parameters.");
-
             }
         }
 
-        return new Parameters(operationType, resource, endpoint);
+        return parametersBuilder.build();
     }
 
     private static String getLocalIpAddress() throws Exception {
--- src/cz/vutbr/fit/openmrdp/Parameters.java
+++ src/cz/vutbr/fit/openmrdp/Parameters.java
@@ -1,5 +1,6 @@
 package cz.vutbr.fit.openmrdp;
 
+import com.google.common.base.Preconditions;
 import com.sun.istack.internal.Nullable;
 import cz.vutbr.fit.openmrdp.messages.OperationType;
 
@@ -10,15 +11,71 @@ import cz.vutbr.fit.openmrdp.messages.OperationType;
 final class Parameters {
     private final OperationType operationType;
 
+    @Nullable
     private final String resource;
 
     @Nullable
     private final String endpoint;
 
-    Parameters(OperationType operationType, String resource, @Nullable String endpoint) {
-        this.operationType = operationType;
-        this.resource = resource;
-        this.endpoint = endpoint;
+    @Nullable
+    private final String fileName;
+
+    @Nullable
+    private final String userName;
+
+    @Nullable
+    private final String password;
+
+    private Parameters(Builder builder) {
+        this.operationType = Preconditions.checkNotNull(builder.operationType);
+        this.resource = builder.resource;
+        this.endpoint = builder.endpoint;
+        this.fileName = builder.fileName;
+        this.userName = builder.userName;
+        this.password = builder.password;
+    }
+
+    static class Builder{
+        private OperationType operationType;
+        private String resource;
+        private String endpoint;
+        private String fileName;
+        private String userName;
+        private String password;
+
+        Builder withOperationType(OperationType operationType){
+            this.operationType = operationType;
+            return this;
+        }
+
+        Builder withResource(String resource){
+            this.resource = resource;
+            return this;
+        }
+
+        Builder withEndpoint(String endpoint){
+            this.endpoint = endpoint;
+            return this;
+        }
+
+        Builder withFileName(String fileName){
+            this.fileName = fileName;
+            return this;
+        }
+
+        Builder withUserName(String userName){
+            this.userName = userName;
+            return this;
+        }
+
+        Builder withPassword(String password){
+            this.password = password;
+            return this;
+        }
+
+        Parameters build(){
+            return new Parameters(this);
+        }
     }
 
     OperationType getOperationType() {
@@ -31,5 +88,17 @@ final class Parameters {
 
     String getEndpoint() {
         return endpoint;
+    }
+
+    String getFileName() {
+        return fileName;
+    }
+
+    String getUserName() {
+        return userName;
+    }
+
+    String getPassword() {
+        return password;
     }
 }
